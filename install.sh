be019cabed26556387574ceedbb2b0012ac2e5e1
mixmix <mix@protozoa.nz>
2023-12-04 14:19:50 +1300

    add install/ setup files

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install \
+  git curl wget zsh neovim ripgrep keepassxc \
+  make gcc g++ libtool \
+  htop slurm \
+  filelight tree \
+  flameshot gimp inkscape
---
+++ setup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+dotfiles="${HOME}/projects/dotfiles"
+
+# LOGGING /////////////////////////////////////////
+name=""
+function start () {
+  name="$1"
+  echo "   ${name}"
+}
+
+CLEAR="\e[1A\e[K"  # go up one line, clear that line
+# CLEAR=""
+progress=""
+function step () { 
+  progress="${progress}."
+  echo -e "${CLEAR}   ${name}${progress}"
+  # echo -n "."
+}
+function end () {
+  progress=""
+  echo -e "${CLEAR} ✓ ${name}${progress}"
+}
+
+# BACKUP /////////////////////////////////////////
+function backup_initial () {
+  local config="${HOME}/$1"
+  local config_backup="${config}.initial"
+  if [ -f $config ] && [ ! -f $conf_backup ]; then
+    cp $config $config_backup
+  fi
+}
+
+# LINKUP /////////////////////////////////////////
+function linkup () {
+  # $1 is assumed to be the path relative to ~
+  local dest_path="${HOME}/$1"
+  local dest_dir=$(dirname $dest_path)
+  local src_path="${dotfiles}/$1"
+
+  ln -fs $src_path $dest_dir 
+}
+
+
+############################################################
+start "zsh"
+
+cd ~
+if [ ! -e ~/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+step
+
+backup_initial .zshrc
+step
+
+linkup .zshrc
+step
+
+end
+############################################################
+start "neovim"
+
+cd ~
+mkdir -p ~/.config/nvim
+
+backup_initial .config/nvim/init.vim
+step
+
+linkup .config/nvim/init.vim
+step
+
+if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ]; then
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
+step
+
+end
+
+echo "   => to complete setup, run :PlugInstall"
+############################################################
